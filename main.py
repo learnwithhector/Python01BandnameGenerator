4069fd5df3bae00ec5bc25d1eadd930fb702af5b
Hector Mcfoy <learnwithhector@gmail.com>
2022-02-17 16:28:46 +0000

    initial and likely final commit for band name generator

---
+++ main.py
@@ -0,0 +1,5 @@
+print("Welcome to the band name generator. Just a couple of questions.")
+city = input("What city/town/village did you first live in? ")
+pet = input("What was the name of your first pet? ")
+
+print("Your band could be called " + city + pet)
