5574270c0ffb272d44e6bc0e2c05d9b9a955903d
Hector Mcfoy <learnwithhector@gmail.com>
2022-02-17 16:33:02 +0000

    Add new lines so prompt appears on seperate line. Add space between names

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 print("Welcome to the band name generator. Just a couple of questions.")
-city = input("What city/town/village did you first live in? ")
-pet = input("What was the name of your first pet? ")
+city = input("What city/town/village did you first live in?\n")
+pet = input("What was the name of your first pet?\n")
 
-print("Your band could be called " + city + pet)
+print("Your band could be called " + city + " " + pet)
